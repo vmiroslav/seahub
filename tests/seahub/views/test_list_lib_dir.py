bfa260d1a19072ac84eee4e405595009ada4ddeb
ilearnit <helloworld.c@foxmail.com>
2018-01-26 15:02:51 +0800

    view-files-tags-with-tests

--- tests/seahub/views/test_list_lib_dir.py
+++ tests/seahub/views/test_list_lib_dir.py
@@ -4,6 +4,8 @@ import os
 from django.core.urlresolvers import reverse
 
 from seahub.test_utils import BaseTestCase
+from seahub.tags.models import FileTag
+from seaserv import seafile_api
 
 class ListLibDirTest(BaseTestCase):
     def setUp(self):
@@ -21,3 +23,30 @@ class ListLibDirTest(BaseTestCase):
         json_resp = json.loads(resp.content)
         assert self.folder_name == json_resp['dirent_list'][0]['obj_name']
         assert self.repo.name == json_resp['repo_name']
+
+    def test_can_list_file_tags(self):
+        seafile_api.post_empty_file(self.repo.id,
+                                    '/',
+                                    filename='test_tags1.txt',
+                                    username=self.user.username)
+        seafile_api.post_empty_file(self.repo.id,
+                                    '/',
+                                    filename='test_tags2.txt',
+                                    username=self.user.username)
+        FileTag.objects.get_or_create_file_tag(self.repo.id,
+                                               '/', 'test_tags2.txt',
+                                               False, 'tag1',
+                                               self.user.username)
+        FileTag.objects.get_or_create_file_tag(self.repo.id,
+                                               '/', 'test_tags2.txt',
+                                               False, 'tag2',
+                                               self.user.username)
+
+        resp = self.client.get(self.endpoint, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        json_resp = json.loads(resp.content)
+
+        for file in json_resp['dirent_list']:
+            if file['obj_name'] == 'test_tag1.txt':
+                self.assertEqual([], file['tags'])
+            if file['obj_name'] == 'test_tags2.txt':
+                self.assertEqual(['tag1', 'tag2'], file['tags'])
